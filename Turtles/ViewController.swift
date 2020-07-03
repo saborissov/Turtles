431d7a5b559c95d5f77947e75b42407eb23de1eb
17586317 <Borisov.S.Alek@sberbank.ru>
2020-07-03 19:05:53 +0300

    Initial Commit

---
+++ Turtles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Turtles
+//
+//  Created by 17586317 on 03.07.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
